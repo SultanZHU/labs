337e2948259b9bb4750ff788d8b03d8bf822cd09
SultanZHU <149163400+SultanZHU@users.noreply.github.com>
2024-02-01 13:31:36 +0600

    Add files via upload

---
+++ 2lab/lab2.py
@@ -0,0 +1,275 @@
+#_-_-_-_-_-_-_-_-_-_-_-_-_Boolean_-_-_-_-_-_-_-_-_-_-_-_-_
+
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+print(bool("Hello"))
+print(bool(15-15))
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+bool("abc")
+bool(123)
+print(bool(["apple", "cherry", "banana"]))
+
+print(bool(""))
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})  #all of this will be false
+
+class myclass():
+  def __len__(self):
+    return 0
+
+myobj = myclass()
+print(bool(myobj))
+
+class myclass():
+  def __len__(self):
+    return 0
+
+myobj = myclass()
+print(bool(myobj))
+
+def myFunction() :
+  return True
+
+print(myFunction())
+
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
+x = 0
+print(isinstance(x, str)) #проверяет переменную на тип данных
+# в данном случае задается вопрос: х это строка??? (ответ: фолс)
+
+
+
+#_-_-_-_-_-_-_-_-_-_-_-_-_Python Operators_-_-_-_-_-_-_-_-_-_-_-_-_
+
+print(10 + 5) # + addition
+
+print((6 + 3) - (6 + 3))
+
+print(100 + 5 * 3)
+
+print(5 + 4 - 7 + 3)
+
+#_-_-_-_-_-_-_-_-_-_-_-_-_Python Lists_-_-_-_-_-_-_-_-_-_-_-_-_
+
+mylist = ["apple", "banana", "cherry"]
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+list1 = ["abc", 34, True, 40, "male"]
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+thislist.remove("banana")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+del thislist
+
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x for x in fruits if "a" in x]
+
+print(newlist)
+
+newlist = [x for x in fruits if x != "apple"]
+print(newlist)
+
+newlist = [x for x in fruits]
+print(newlist)
+
+newlist = [x.upper() for x in fruits]
+print(newlist)
+
+newlist = ['hello' for x in fruits]
+print(newlist)
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse = True)
+print(thislist)
+
+def myfunc(n):
+  return abs(n - 50)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort()
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+#-_-_-_-_-_-_-_-_-_-_Tuples-_-_-_-_-_-_-_-_-_-_
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
+
+fruits = ("apple", "banana", "cherry")
+
+(green, yellow, red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
