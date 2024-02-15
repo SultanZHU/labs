d2be46aeb683dce645fa907895f965f3df1574ae
SultanZHU <149163400+SultanZHU@users.noreply.github.com>
2024-02-15 13:18:04 +0600

    Add files via upload

---
+++ 4lab/math.py
@@ -0,0 +1,24 @@
+import math
+
+#1
+a = 15
+print(round(a * (math.pi/180), 6)) # ya ot sebya dobavil round
+
+#2
+h = 5
+b1 = 5
+b2 = 6
+print((b1 + b2)/2*h)
+
+#3
+s = 4
+len = 25
+area = (s * len**2) / (4 * math.tan(math.pi / s))
+print(int(area))
+
+#4
+base = 5
+height = 6
+area = base*height
+print(area)
+
